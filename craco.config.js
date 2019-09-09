0879ea2b8d3e571b15e79015f8997e5e7c7bba4e
dattebayorob <robson.william65@gmail.com>
2019-09-09 20:40:11 -0300

    Initial setup with craco

---
+++ craco.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/')
+    }
+  },
+  jest: {
+    configure: {
+      moduleNameMapper: {
+        '^@(.*)$': '<rootDir>/src$1'
+      }
+    }
+  },
+  babel: {
+    plugins: [
+      ["@babel/plugin-proposal-decorators", { "legacy": true }]
+    ]
+  }
+};
---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        Hello World
+      </div>
+    </Router>
+  )
+}
+
+export default App
